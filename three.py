ebfe92bb43869b5f819fe1b398ed14b8095fb8ba
Shahmashanid <ayshathshahmabs0@gmail.com>
2026-07-20 11:34:52 +0530

    Add files via upload

---
+++ three.py
@@ -0,0 +1,48 @@
+#datatypes:
+#classification of data
+#int,float,str,bool,list,tuple,set,dictionary
+#numeric:int(-,0,+),float(0.0)
+
+#integer:
+a=-5
+print(a)
+print(type(a))
+#string:
+name = "mohan"
+print(name)
+print(type(name))
+#float:
+a=9.0
+print(a)
+print(type(a))
+#boolean:
+a=True #if there is value in a variable true 
+b=False #if there is no value in a variable false ,0 is false
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+a=-4
+print(bool(a))
+a=0
+print(bool(a))
+
+#types of operations
+#1. arithamatic operations:+,_,*,/
+a=7.0
+b=9
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) #in div even you get perfect value but python will give float value
+#exponents(raise to/power):**
+print(2**3)#2*2*2=8
+#floor division: // gives quotient
+print(9//4)#9/4=2.25 which is btwn 2 and 3, go to small value 2,used to get number not in point
+#modulus: % gives remainder
+print(10%7)#reminder=3
+
+
+
+
+
